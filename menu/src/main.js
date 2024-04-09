589bc209514d51d500db3e0e918cb8d550598f2d
Naza <nazadoto@gmail.com>
2024-04-09 09:58:55 -0300

    aber

--- menu/src/main.js
+++ menu/src/main.js
@@ -6,7 +6,7 @@ import router from './router';
 const env = 'dev';
 
 if (env == 'dev') {
-    axios.defaults.baseURL = 'http://nazadoto.com:3500';
+    axios.defaults.baseURL = 'http://vps-4028484-x.dattaweb.com:3500';
 } else {
 
     axios.defaults.baseURL = 'https://nazadoto.com:3500';
