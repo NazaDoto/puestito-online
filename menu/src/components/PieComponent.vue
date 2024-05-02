ba5c0f1e52e80830c2b750e43cd06e4b11c2ba2d
Naza <nazadoto@gmail.com>
2024-05-02 11:19:38 -0300

    futer

--- menu/src/components/PieComponent.vue
+++ menu/src/components/PieComponent.vue
@@ -1,8 +1,12 @@
 <template>
     <footer class="text-center">
   <!-- Copyright -->
-  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-    © 2024 <a class="text-dark" href="https://puestito.online/">Puestito Online</a>
+  <div class="text-center p-2" style="background-color: rgba(0, 0, 0, 0.2);">
+    <a href="https://puestito.online/"><img src="favicon.ico" width="22" alt=""></a>
+    <div style="margin-left:10px; display: inline;">
+      Puestito Online
+    </div>
+    <a style="margin-left:10px;" href="http://instagram.com/puestito.online" target="_blank"><img src="/recursos/instagram.png" width="20" alt=""></a>
   </div>
   <!-- Copyright -->
 </footer>
