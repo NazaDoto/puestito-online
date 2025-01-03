7ebc049fac45189ccfb58cf7c942cd0399930cbd
Naza <nazadoto@gmail.com>
2025-01-03 11:53:36 -0300

    xd

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const corsOptions = {
 
 // Configurar la app de Express
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3600;
 
 // Middleware para solicitudes preflight
 app.options('*', cors(corsOptions));
