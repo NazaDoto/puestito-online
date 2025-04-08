af9b00930c2284095cc774b46d331ad903cd8d83
Naza <nazadoto@gmail.com>
2025-04-07 21:35:43 -0300

    e

--- puestito-online/vue.config.js
+++ puestito-online/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
-    publicPath: '/puestito-online/',
+    publicPath: '/puestito-online/'
 })
