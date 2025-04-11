cc5fcb59acd0166cddc98b2096e49feff51ec9f3
Naza <nazadoto@gmail.com>
2025-04-11 09:47:50 -0300

    xd

--- puestito-online/src/components/InicioComponent.vue
+++ puestito-online/src/components/InicioComponent.vue
@@ -19,7 +19,7 @@
                   <path fill-rule="evenodd"
                     d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z" />
                 </svg></router-link>
-              <router-link v-else c lass="btn-login inline-end c-white" to="/u/login">Ingresar <svg
+              <router-link v-else class="btn-login inline-end c-white" to="/u/login">Ingresar <svg
                   xmlns="http://www.w3.org/2000/svg" width="28" fill="currentColor" class="bi bi-box-arrow-in-right"
                   viewBox="0 0 16 16">
                   <path fill-rule="evenodd"
